842eb4c369f4ada4afbf92a18180b1014ceb8d77
Obazure <conprefor@gmail.com>
2018-08-23 18:41:38 +0600

    1.2.2.ProblemC-FibHugeNumDivM solved on Java

--- 1.2.2.ProblemC-FibHugeNumDivM/java/app.java
+++ 1.2.2.ProblemC-FibHugeNumDivM/java/app.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 class Main {
-  public static int fibonacci(int n, int m) {
-		int f1 = 0;
-		int f2 = 1, f3 = 1;
-		for (int i = 1; i < n; i++){
-			f3 = f1 + f2;
-			f1 = f2;
-			f2 = f3 % m;
-		}
-        return f2;
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scaner = new Scanner(System.in);
+        long n = scaner.nextLong();
+        // max int == 2147483647 > 100000.0
+        int m = scaner.nextInt();
+        System.out.println(modfib(n, m));
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        System.out.println(fibonacci(n,m));
+    private static int modfib(long n, int m) {
+        if (n >= 1 && m >= 2 && m <= Math.pow(10, 5) && n <= Math.pow(10, 18)) {
+            int[] mas = new int[m * 6];
+            mas[0] = 0;
+            mas[1] = 1;
+            for (int i = 2; i <= mas.length; i++) {
+                if ((mas[i] = ((mas[i - 1] + mas[i - 2]) % m)) == 1 && mas[i - 1] == 0) {
+                    return mas[(int) (n % (i - 1))];
+                }
+            }
+        }
+        return 0;
     }
 }
